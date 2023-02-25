b2c2f9776c04b3685954186bf22f18cd73072fcb
khandarev.int <khandarev.int@media.net>
2023-02-25 16:06:38 +0530

    save

--- client/src/util/asyncAPIMethods.js
+++ client/src/util/asyncAPIMethods.js
@@ -2,8 +2,8 @@ export const getAPI = (url,params)=> {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-    return fetch(`${url}?${new URLSearchParams(params || {})}`,{headers})
+    headers.append('Access-Control-Allow-Origin', 'https://regal-selkie-753848.netlify.app/');
+    return fetch(`https://ecommerce-q1md.onrender.com${url}?${new URLSearchParams(params || {})}`,{headers})
     .then(res=>res.json())
     .catch(err=>console.log('error', err))
 }
@@ -11,7 +11,7 @@ export const getAPI = (url,params)=> {
 export const postAPI = (url,body)=> {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return fetch(`${url}`,{
+    return fetch(`https://ecommerce-q1md.onrender.com${url}`,{
         method: "POST",
     body: JSON.stringify(body),
     headers: {
